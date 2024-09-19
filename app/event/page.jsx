83081a07bca68b63b90ec259616e0594f030b617
Ekta Chaudhary <ektachaudhary129@gmail.com>
2024-09-19 22:56:27 +0530

    WIP: events page

---
+++ app/event/page.jsx
@@ -0,0 +1,30 @@
+"use client";
+import { useEffect, useState } from "react";
+import EventCard from "@/components/EventCard";
+function EventsPage() {
+  const [eventsData, setEventData] = useState([]);
+
+  async function fetchData() {
+    const eventReq = await fetch("https://qevent-backend.labs.crio.do/events");
+
+    const responseData = await eventReq.json();
+    console.log(responseData);
+    setEventData(responseData);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <div style={{ display: "flex", flexWrap: "wrap", padding: "32px" }}>
+      {eventsData.map((event) => (
+        <div style={{ flex: "1 1 350px" }}>
+          <EventCard key={event.id} eventData={event} />
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default EventsPage;
--- components/EventCard.jsx
+++ components/EventCard.jsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 const EventCard = ({ eventData }) => {
   return (
-    <div className="hover-inverse w-[30%] h-fit group transform transition-transform duration-400 hover:scale-110 hover:bg-gradient-to-r hover:from-orange-200 hover:to-white text-dark m-4 border-slate-400 border rounded-md px-8 py-2.5">
+    <div className="hover-inverse h-fit group transform transition-transform duration-400 hover:scale-110 hover:bg-gradient-to-r hover:from-orange-200 hover:to-white text-dark m-4 border-slate-400 border rounded-md px-8 py-2.5">
       <Link
         href={`#`}
         className="rounded-md text-dark flex-shrink-0 scroll-snap-card p-4"
